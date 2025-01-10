0f68d758867b3bf4ffeb3343f7b96c66c549b815
TinuBasil <tinu.basil@dcmail.ca>
2025-01-10 15:31:11 -0500

    First Push

---
+++ src/client/app/animals/delete.js
@@ -0,0 +1,7 @@
+/*
+    Name: Tinu Basil
+    filename: app.js
+    Course: INFT 2202
+    Date: 10 jan 2025
+    Description: This is my general application script.  Functions that are required on every page should live here.
+*/
